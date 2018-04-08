988dcd24536e4a2afa478815d9fe6fbf7a3c1dd8
williambl <william@williambl.com>
2018-04-08 14:06:42 +0100

    Added player health

---
+++ linuxgamejam2018/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int health = 5;
+
+    // Update is called once per frame
+    void Update () {
+		
+    }
+
+    public void RemoveHealth() {
+        health--;
+    }
+}
