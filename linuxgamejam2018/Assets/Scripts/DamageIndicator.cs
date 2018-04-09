d1bc84e732a5622e94e356a875094515a7a33024
williambl <william@williambl.com>
2018-04-09 17:17:10 +0100

    Damage indicators now fly away

---
+++ linuxgamejam2018/Assets/Scripts/DamageIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageIndicator : MonoBehaviour {
+
+    int timer = 160;
+
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        if (timer == 0)
+            Destroy(gameObject);
+
+        transform.position = (Vector2)transform.position + new Vector2(0, 0.1f);
+        timer--;
+    }
+}
