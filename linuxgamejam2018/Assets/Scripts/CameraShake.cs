e836975864387f52410874c968ea00b7d5895ae4
williambl <william@williambl.com>
2018-04-09 17:51:59 +0100

    Added a camera shake script

---
+++ linuxgamejam2018/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    //Based on https://forum.unity.com/threads/screen-shake-effect.22886/#post-153233
+    Camera cam;
+    float shakiness = 0f;
+    float overallShakiness = 0.4f;
+    float shakeDecrease = 3.0f;
+    Vector3 originalCameraPos;
+
+    // Use this for initialization
+    void Start () {
+        cam = Camera.main;
+        originalCameraPos = cam.transform.localPosition;        
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        if (shakiness > 0)  {
+            Vector2 rawShake = Random.insideUnitCircle * overallShakiness;
+            cam.transform.localPosition = new Vector3(rawShake.x, rawShake.y, -10);
+            shakiness -= Time.deltaTime * shakeDecrease;
+        } else {
+            shakiness = 0f;
+            cam.transform.localPosition = originalCameraPos;
+        }
+    }
+}
