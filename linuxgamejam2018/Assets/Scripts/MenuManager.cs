02ad1c065f6a620c6993e80aaa97683c54914c6b
williambl <william@williambl.com>
2018-04-10 13:10:15 +0100

    Added a basic menu

---
+++ linuxgamejam2018/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public void StartGame () {
+        SceneManager.LoadScene("game");
+    }
+
+    public void Exit () {
+        Application.Quit();
+    }
+}
