b5b4c47f6400621b3d6d2f0606310984399f8a52
williambl <william@williambl.com>
2018-04-12 15:10:12 +0100

    Added a game manager

---
+++ linuxgamejam2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager manager;
+
+    public GameObject winCanvas;
+
+    void Start () {
+        GameManager.manager = this;
+    }
+
+    public void Win () {
+        winCanvas.SetActive(true);
+    }
+
+    public void ReturnToMenu () {
+        SceneManager.LoadScene("menu");
+    }
+}
