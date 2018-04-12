93ce58c579848b6e366f24d2d1ee15c689cbdcdb
williambl <william@williambl.com>
2018-04-12 10:52:42 +0100

    Added a Death Zone below the level

---
+++ linuxgamejam2018/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Health health = other.GetComponent<Health>();
+        if (health == null)
+            return;
+
+        PlayerHealth pHealth = other.GetComponent<PlayerHealth>();
+        EnemyHealth eHealth = other.GetComponent<EnemyHealth>();
+        if (pHealth != null)
+            pHealth.RemoveHealth(999);
+        else if (eHealth != null)
+            eHealth.RemoveHealth(999);
+    }
+}
