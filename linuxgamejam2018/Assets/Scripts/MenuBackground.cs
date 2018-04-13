b0006ef81712c003cd8b1543ede11111556559c2
williambl <william@williambl.com>
2018-04-13 22:33:20 +0100

    Added scrolling background to menu

---
+++ linuxgamejam2018/Assets/Scripts/MenuBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackground : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        transform.position = (Vector2)transform.position - new Vector2(0, 0.1f);
+
+        if (transform.position.y < -11)
+            transform.position = new Vector2(transform.position.x, 36.3f);
+    }
+}
